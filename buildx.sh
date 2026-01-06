fd4ad90430d754cc539c6269abe427933f47e1a0
LynxSaiko <redteamsectorid@gmail.com>
2026-01-06 13:42:20 +0700

    Update buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -18,7 +18,7 @@ mkdir -p $INITRD_TREE/{bin,dev,etc,lib,lib64,mnt/media,mnt/squash,mnt/rw,mnt/mer
 echo "[1/4] Membuat SquashFS dari $LFS_MOUNT..."
 # Pastikan mount point media ada di dalam LFS agar sistem punya target mount nanti
 sudo mkdir -p $LFS_MOUNT/mnt/media
-sudo mksquashfs $LFS_MOUNT $ISO_ROOT/sources/rootfs.squashfs -comp xz -e boot proc sys dev tmp mnt/media
+sudo mksquashfs $LFS_MOUNT $ISO_ROOT/sources/rootfs.squashfs -comp xz -all-root -e boot proc sys dev tmp mnt/media usr/lib/python3.10 var/log var/cache home/*
 
 # 3. Siapkan Initramfs (Initrd)
 echo "[2/4] Menyiapkan Initramfs dengan BusyBox..."
