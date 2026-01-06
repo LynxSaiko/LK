1b77e26fcec5b2ef72ba3901405510d52b1e194c
LynxSaiko <redteamsectorid@gmail.com>
2026-01-06 22:28:39 +0700

    Add command to create ISO image using grub-mkrescue

--- grub.sh
+++ grub.sh
@@ -5,3 +5,4 @@
 # ==========================================================
 WORK_DIR="~/LeakOS-Build"    # Folder pusat pengerjaan
 ISO_ROOT="$WORK_DIR/iso_root"       # Folder struktur akhir ISO
+grub-mkrescue -o $WORK_DIR/lfs-live.iso $ISO_ROOT -iso-level 3
