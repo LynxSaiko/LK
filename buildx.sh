77e595fe8508db2f39f87c87d2fd84841254da66
LynxSaiko <redteamsectorid@gmail.com>
2026-01-06 16:26:45 +0700

    Update buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -47,7 +47,7 @@ sudo mknod -m 666 $INITRD_TREE/dev/null c 1 3
 
 # Buat Skrip init di dalam folder initrd_tree
 cat << 'EOF' > $INITRD_TREE/init
-#!/bin/sh
+#!/bin/busybox sh
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
 mount -t devtmpfs devtmpfs /dev
