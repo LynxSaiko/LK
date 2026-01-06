d23e736fb4f1df3348447696151d22fa9787db90
LynxSaiko <redteamsectorid@gmail.com>
2026-01-06 22:27:38 +0700

    Create grub.sh

---
+++ grub.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# ==========================================================
+# KONFIGURASI - SESUAIKAN JALUR DI SINI
+# ==========================================================
+WORK_DIR="~/LeakOS-Build"    # Folder pusat pengerjaan
+ISO_ROOT="$WORK_DIR/iso_root"       # Folder struktur akhir ISO
